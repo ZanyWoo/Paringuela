04cfeff29c50955da778b34e0f9cf5ed7ef71e29
ZanyWoo <163156962+ZanyWoo@users.noreply.github.com>
2024-03-12 12:12:56 -0300

    Add files via upload

---
+++ aula1.py
@@ -0,0 +1,75 @@
+Numeros = int(input("Quantos numeros voce quer?\n"))
+
+
+if Numeros == 2 :
+    valorA = int(input("ESCREVA ESSA ME... COISA LEGAL! \n"))
+    valorB = int(input("DENOVO PORQUE MELHOR Q 1 É 2 \n"))
+
+    Operacao = int(input("Cada numero é uma operação diferente \n 1 - Soma \n 2 - Subtração \n 3 - Divisão \n 4 - Multiplicação \n"))
+
+    if Operacao == 1 :
+        print("A soma é", valorA + valorB)
+    elif Operacao == 2 :
+        print("A subtração é", valorA - valorB)
+    elif Operacao == 3 :
+        print("a Divisão é", valorA / valorB)
+    elif Operacao == 4 :
+        print("a Multiplicação é", valorA * valorB)
+    else :
+        print("PARO COM AS BRINCADEIRA \n SEU SACANA \n VO LEMBRA DISSO \n QUE TU FICA ME ZOANDO AE")
+elif Numeros == 3 :
+    valorA = int(input("ESCREVA ESSA ME... COISA LEGAL! \n"))
+    valorB = int(input("DENOVO PORQUE MELHOR Q 1 É 2 \n"))
+    valorC = int(input("AGORA O TERCEIRO \n"))
+
+    Operacao = int(input("Cada numero é uma operação diferente \n 1 - Soma \n 2 - Subtração \n 3 - Divisão \n 4 - Multiplicação \n"))
+
+    if Operacao == 1 :
+        print("A soma é", valorA + valorB + valorC)
+    elif Operacao == 2 :
+        print("A subtração é", valorA - valorB - valorC)
+    elif Operacao == 3 :
+        print("a Divisão é", valorA / valorB / valorC)
+    elif Operacao == 4 :
+        print("a Multiplicação é", valorA * valorB * valorC)
+    else :
+        print("PARO COM AS BRINCADEIRA \n SEU SACANA \n VO LEMBRA DISSO \n QUE TU FICA ME ZOANDO AE")
+elif Numeros == 4 :
+    valorA = int(input("ESCREVA ESSA ME... COISA LEGAL! \n"))
+    valorB = int(input("DENOVO PORQUE MELHOR Q 1 É 2 \n"))
+    valorC = int(input("AGORA O TERCEIRO \n"))
+    valorD = int(input("AGORA O QUARTO \n"))
+
+    Operacao = int(input("Cada numero é uma operação diferente \n 1 - Soma \n 2 - Subtração \n 3 - Divisão \n 4 - Multiplicação \n"))
+
+    if Operacao == 1 :
+        print("A soma é", valorA + valorB + valorC + valorD)
+    elif Operacao == 2 :
+        print("A subtração é", valorA - valorB - valorC - valorD)
+    elif Operacao == 3 :
+        print("a Divisão é", valorA / valorB / valorC / valorD)
+    elif Operacao == 4 :
+        print("a Multiplicação é", valorA * valorB * valorC * valorD)
+    else :
+        print("PARO COM AS BRINCADEIRA \n SEU SACANA \n VO LEMBRA DISSO \n QUE TU FICA ME ZOANDO AE")
+elif Numeros == 5 :
+    valorA = int(input("ESCREVA ESSA ME... COISA LEGAL! \n"))
+    valorB = int(input("DENOVO PORQUE MELHOR Q 1 É 2 \n"))
+    valorC = int(input("AGORA O TERCEIRO \n"))
+    valorD = int(input("AGORA O QUARTO \n"))
+    valorE = int(input("AGORA O QUINTO \n"))
+
+    Operacao = int(input("Cada numero é uma operação diferente \n 1 - Soma \n 2 - Subtração \n 3 - Divisão \n 4 - Multiplicação \n"))
+
+    if Operacao == 1 :
+        print("A soma é", valorA + valorB + valorC + valorD + valorE)
+    elif Operacao == 2 :
+        print("A subtração é", valorA - valorB - valorC - valorD - valorE)
+    elif Operacao == 3 :
+        print("a Divisão é", valorA / valorB / valorC / valorD / valorE)
+    elif Operacao == 4 :
+        print("a Multiplicação é", valorA * valorB * valorC * valorD * valorE)
+    else :
+        print("PARO COM AS BRINCADEIRA \n SEU SACANA \n VO LEMBRA DISSO \n QUE TU FICA ME ZOANDO AE")
+else :
+    print("Numero Invalido")
